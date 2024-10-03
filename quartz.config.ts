55be45f21ca89eb794aabdc92e20f9dd62a37411
ewanpedersenUVM <ejpeders@uvm.edu>
2024-10-03 01:13:51 -0400

    Quartz sync: Oct 3, 2024, 1:13 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import { Staticrypt } from "./quartz/password"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "GΛSSAИDRID.XYZ",
+    pageTitle: "GΛSSAИDRΙϽ.ΧΨZ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
